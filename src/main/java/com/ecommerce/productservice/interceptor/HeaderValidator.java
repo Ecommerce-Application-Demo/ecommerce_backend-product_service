95d92bf5608bb20368951a384fe76413ce1ff5dd
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-04-29 13:00:59 +0530

    Secured Post APIs with API key & secret

---
+++ src/main/java/com/ecommerce/productservice/interceptor/HeaderValidator.java
@@ -0,0 +1,21 @@
+package com.ecommerce.productservice.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.coyote.BadRequestException;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HeaderValidator {
+
+    public static void requestValidator(HttpServletRequest request,String apiKey, String apiSecret) throws BadRequestException {
+        List<String> requestHeaders = Collections.list(request.getHeaderNames());
+        if(!requestHeaders.contains(apiKey)){
+            throw new BadRequestException("API Key is not passed");
+        } else {
+            if(!request.getHeader(apiKey).equals(apiSecret)){
+                throw new BadRequestException("API Secret is Invalid");
+            }
+        }
+    }
+}
